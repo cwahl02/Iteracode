acf486d48611d07ad8d61d8dce2a4947cd6ada09
cwahl02 <cwismgitism@gmail.com>
2026-03-03 23:33:56 -0800

    Enhance endpoint registration by adding overload for assembly and improving method signatures

--- Iteracode.Api/Extensions/EndpointExtensions.cs
+++ Iteracode.Api/Extensions/EndpointExtensions.cs
@@ -5,26 +5,33 @@ namespace Iteracode.Api.Extensions;
 
 public static class EndpointExtensions
 {
+    public static IServiceCollection AddEndpoints(this IServiceCollection services)
+    {
+        AddEndpoints(services, Assembly.GetExecutingAssembly());
+        return services;
+    }
+
     public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
     {
         var endpointTypes = assembly
             .GetTypes()
             .Where(t => typeof(IEndpoint).IsAssignableFrom(t) && t is { IsAbstract: false, IsClass: true });
 
-        foreach (var endpointType in endpointTypes)
+        foreach (var type in endpointTypes)
         {
-            services.AddSingleton(typeof(IEndpoint), endpointType);
+            services.AddSingleton(typeof(IEndpoint), type);
         }
 
         return services;
     }
 
-    public static IApplicationBuilder MapEndpoints(this WebApplication app)
+    public static IApplicationBuilder MapEndpoints(this WebApplication app, RouteGroupBuilder? routeGroupBuilder = null)
     {
-        var endpoints = app.Services.GetServices<IEndpoint>();
+        IEnumerable<IEndpoint> endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+        IEndpointRouteBuilder builder = routeGroupBuilder is null ? app : routeGroupBuilder;
         foreach (var endpoint in endpoints)
         {
-            endpoint.MapEndpoint(app);
+            endpoint.MapEndpoint(builder);
         }
         return app;
     }
