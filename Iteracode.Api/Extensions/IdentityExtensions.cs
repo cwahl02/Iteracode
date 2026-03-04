1562907f814b3d16ef2cfe376d443ba250c6167d
cwahl02 <cwismgitism@gmail.com>
2026-03-03 23:34:03 -0800

    Implement identity services registration with user and role configurations

--- Iteracode.Api/Extensions/IdentityExtensions.cs
+++ Iteracode.Api/Extensions/IdentityExtensions.cs
@@ -1,3 +1,7 @@
+using Iteracode.Api.Data;
+using Iteracode.Api.Models;
+using Microsoft.AspNetCore.Identity;
+
 namespace Iteracode.Api.Extensions;
 
 public static class IdentityExtensions
@@ -6,22 +10,23 @@ public static class IdentityExtensions
     {
         // TODO: Move Identity options to appsettings.json and bind them to a strongly typed class
         // Uncomment when User model, ApplicationDbContext and other Identity related classes are implemented
-        // services.AddIdentity<>(options =>
-        // {
-        //     options.Password.RequireDigit = true;
-        //     options.Password.RequireLowercase = true;
-        //     options.Password.RequireUppercase = true;
-        //     options.Password.RequireNonAlphanumeric = false;
-        //     options.Password.RequiredLength = 6;
+        services.AddIdentity<User, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 6;
 
-        //     options.User.RequireUniqueEmail = true;
+            options.User.RequireUniqueEmail = true;
+            options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
 
-        //     // In future move lockout settings to appsettings.json
-        //     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-        //     options.Lockout.MaxFailedAccessAttempts = 5;
-        // })
-        // .AddEntityFrameworkStores<DataContext>()
-        // .AddDefaultTokenProviders();
+            // In future move lockout settings to appsettings.json
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
 
         return services;
     }
