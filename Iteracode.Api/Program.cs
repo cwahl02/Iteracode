0a68601f258bf1495bb838605f2c379bafc5f7da
cwahl02 <cwismgitism@gmail.com>
2026-03-09 02:59:57 -0700

    feat: add Judge0 service integration with HTTP client configuration

--- Iteracode.Api/Program.cs
+++ Iteracode.Api/Program.cs
@@ -2,6 +2,8 @@ using FluentValidation;
 using Iteracode.Api.Extensions;
 using Iteracode.Api.Options;
 using System.Reflection;
+using Iteracode.Api.Services;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +18,18 @@ builder.Services
     .AddEndpoints()
     .AddValidatorsFromAssembly(typeof(Program).Assembly)
     .AddInjectionMarkers()
-    .AddAuthorization();
+    .AddAuthorization()
+    .AddJudge0(builder.Configuration);
+
+builder.Services.AddHttpClient<IJudge0Service, Judge0Service>((provider, client) =>
+{
+    var options = provider.GetRequiredService<IOptions<Judge0Options>>().Value;
+    client.BaseAddress = new Uri(options.BaseUrl);
+
+    if (!string.IsNullOrEmpty(options.AuthToken))
+        client.DefaultRequestHeaders.Add("X-Auth-Token", options.AuthToken);
+
+});
 
 var app = builder.Build();
 
